bf91272ce4fc2935d39ba06c626a006b9a4c5028
Adeleke Ayinde <lekea4@gmail.com>
2021-11-18 14:34:48 +0100

    Modify route-config to enable routing by id

--- src/route-config.ts
+++ src/route-config.ts
@@ -13,25 +13,27 @@ import EditCinema from "./cinemas/EditCinema";
 import CreateMovie from "./movies/CreateMovie";
 import EditMovie from "./movies/EditMovie";
 import FilterMovies from "./movies/FilterMovies";
+import RedirectToLandingPage from "./utils/RedirectToLandingPage";
 
 const routes = [
   { path: "/genres", component: IndexGenres, exact: true },
   { path: "/genres/create", component: CreateGenres },
-  { path: "/genres/edit", component: EditGenres },
+  { path: "/genres/edit/:id(\\d+)", component: EditGenres },
 
   { path: "/actors", component: IndexActors, exact: true },
   { path: "/actors/create", component: CreateActor },
-  { path: "/actors/edit", component: EditActor },
+  { path: "/actors/edit/:id(\\d+)", component: EditActor },
 
   { path: "/cinemas", component: IndexCinemas, exact: true },
   { path: "/cinemas/create", component: CreateCinema },
-  { path: "/cinemas/edit", component: EditCinema },
+  { path: "/cinemas/edit/:id(\\d+)", component: EditCinema },
 
   { path: "/movies/create", component: CreateMovie },
-  { path: "/movies/edit", component: EditMovie },
+  { path: "/movies/edit/:id(\\d+)", component: EditMovie },
   { path: "/movies/filter", component: FilterMovies },
 
   { path: "/", component: LandingPage, exact: true },
+  { path: "*", component: RedirectToLandingPage },
 ];
 
 export default routes;
