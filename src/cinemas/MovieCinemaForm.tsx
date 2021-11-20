d594bf1711834ad507ca75e3e3c82e0073076818
Adeleke Ayinde <lekea4@gmail.com>
2021-11-20 16:55:11 +0100

    Create the cinema form component to implement the cinema model interface

---
+++ src/cinemas/MovieCinemaForm.tsx
@@ -0,0 +1,46 @@
+import { Form, Formik, FormikHelpers } from "formik";
+import { Link } from "react-router-dom";
+import TextField from "../forms/TextField";
+import Button from "../utils/Button";
+import { movieCinemaCreationDTO } from "./movieCinema.model";
+import * as Yup from "yup";
+
+export default function MovieCinemaForm(props: movieCinemaForm) {
+  return (
+    <Formik
+      initialValues={props.model}
+      onSubmit={props.onSubmit}
+      validationSchema={Yup.object({
+        _name: Yup.string()
+          .required("This field is required")
+          .firstLetterUppercase(),
+        get name() {
+          return this._name;
+        },
+        set name(value) {
+          this._name = value;
+        },
+      })}
+    >
+      {(formikProps) => (
+        <Form>
+          <TextField displayName="Name" field="name" />
+          <Button disabled={formikProps.isSubmitting} type="submit">
+            Save
+          </Button>
+          <Link className="btn btn-secondary" to="/cinenas">
+            Cancel
+          </Link>
+        </Form>
+      )}
+    </Formik>
+  );
+}
+
+interface movieCinemaForm {
+  model: movieCinemaCreationDTO;
+  onSubmit(
+    values: movieCinemaCreationDTO,
+    actions: FormikHelpers<movieCinemaCreationDTO>
+  ): void;
+}
