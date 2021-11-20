4a66d29adb823fd45a3075efc600be6f82eb266c
Adeleke Ayinde <lekea4@gmail.com>
2021-11-20 16:56:15 +0100

    Impelement the Cinema form in Create and edit component

--- src/cinemas/CreateCinema.tsx
+++ src/cinemas/CreateCinema.tsx
@@ -1,7 +1,13 @@
+import MovieCinemaForm from "./MovieCinemaForm";
+
 export default function CreateCinema() {
   return (
     <>
       <h3>Create Cinema</h3>
+      <MovieCinemaForm
+        model={{ name: "" }}
+        onSubmit={(values) => console.log(values)}
+      />
     </>
   );
 }
--- src/cinemas/EditCinema.tsx
+++ src/cinemas/EditCinema.tsx
@@ -1,7 +1,13 @@
+import MovieCinemaForm from "./MovieCinemaForm";
+
 export default function EditCinema() {
   return (
     <>
       <h3>Edit Cinema</h3>
+      <MovieCinemaForm
+        model={{ name: "Filmhouse" }}
+        onSubmit={(values) => console.log(values)}
+      />
     </>
   );
 }
