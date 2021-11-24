1c0b3ad336e7ea92daa7219110fd1a30b1bd46e8
Adeleke Ayinde <lekea4@gmail.com>
2021-11-24 17:48:37 +0100

    Modify the IndexGenre Component to impelement the genric IndexEntity component

--- src/genres/IndexGenres.tsx
+++ src/genres/IndexGenres.tsx
@@ -1,46 +1,35 @@
-import axios, { AxiosResponse } from "axios";
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import { urlGenres } from "../endpoints";
-import Button from "../utils/Button";
-import GenericList from "../utils/GenericList";
+import IndexEntity from "../utils/IndexEntity";
 import { genreDTO } from "./genres.model";
 
 export default function IndexGenres() {
-  const [genres, setGenres] = useState<genreDTO[]>([]);
-  useEffect(() => {
-    axios
-      .get(urlGenres)
-      .then((response: AxiosResponse<genreDTO>) => console.log(response.data));
-  }, []);
   return (
     <>
-      <h3>Genres</h3>
-      <Link className="btn btn-primary" to="/genres/create">
-        Create Genre
-      </Link>
-      <GenericList list={genres}>
-        <table className="table table-striped">
-          <thead>
-            <th>Name</th>
-            <th></th>
-          </thead>
-          <tbody>
-            {genres?.map((genre) => (
-              <tr key={genre.id}>
-                <td>{genre.name}</td>
-                <td>
-                  <Link className="btn btn-success" to={`/genres/${genre.id}`}>
-                    Edit
-                  </Link>
-
-                  <Button className="btn btn-danger">Delete</Button>
-                </td>
+      <IndexEntity<genreDTO>
+        url={urlGenres}
+        createURL="genres/create"
+        title="Genres"
+        entityName="Gnere"
+      >
+        {(genres, buttons) => (
+          <>
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </GenericList>
+            </thead>
+            <tbody>
+              {genres?.map((genre) => (
+                <tr key={genre.id}>
+                  <td>{buttons(`genres/edit/${genre.id}`, genre.id)}</td>
+                  <td>{genre.name}</td>
+                </tr>
+              ))}
+            </tbody>
+          </>
+        )}
+      </IndexEntity>
     </>
   );
 }
