0bdec1ece9589ef16bf6912d73242ce1f751f52a
Adeleke Ayinde <lekea4@gmail.com>
2021-11-29 10:10:16 +0100

    Modify the App component to implement Authorization

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,48 @@ import Menu from "./Menu";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import routes from "./route-config";
 import configureValidation from "./Validations";
+import { claim } from "./auth/auth.models";
+import { useState } from "react";
+import AuthenticationContext from "./auth/AuthenticationContext";
 
 configureValidation();
 
 function App() {
+  const [claims, setClaims] = useState<claim[]>([
+    { name: "email", value: "lekea4@gmail.com" },
+  ]);
+
+  function isAdmin() {
+    return (
+      claims.findIndex(
+        (claim) => claim.name === "role" && claim.value === "admin"
+      ) > -1
+    );
+  }
   return (
     <BrowserRouter>
-      <Menu />
+      <AuthenticationContext.Provider value={{ claims, update: setClaims }}>
+        <Menu />
 
-      <div className="container">
-        <Switch>
-          {routes.map((route) => (
-            <Route key={route.path} path={route.path} exact={route.exact}>
-              <route.component />
-            </Route>
-          ))}
-        </Switch>
-      </div>
-      <footer className="bd-footer py-5 mt-5 bg-light border-top footer text-muted">
-        <div className="container-fluid text-center">
-          &copy;{new Date().getFullYear().toString()} - MovieBox
+        <div className="container">
+          <Switch>
+            {routes.map((route) => (
+              <Route key={route.path} path={route.path} exact={route.exact}>
+                {route.isAdmin && !isAdmin() ? (
+                  <>You are not allowed to view this page</>
+                ) : (
+                  <route.component />
+                )}
+              </Route>
+            ))}
+          </Switch>
         </div>
-      </footer>
+        <footer className="bd-footer py-5 mt-5 bg-light border-top footer text-muted">
+          <div className="container-fluid text-center">
+            &copy;{new Date().getFullYear().toString()} - MovieBox
+          </div>
+        </footer>
+      </AuthenticationContext.Provider>
     </BrowserRouter>
   );
 }
