4927c30d5cc9db978d13b0aef8615d8b7172c648
Adeleke Ayinde <lekea4@gmail.com>
2021-11-29 10:11:50 +0100

    Modify the route-config  class  to implement Authorization on some routes

--- src/route-config.ts
+++ src/route-config.ts
@@ -17,20 +17,29 @@ import MovieDetails from "./movies/MovieDetails";
 import RedirectToLandingPage from "./utils/RedirectToLandingPage";
 
 const routes = [
-  { path: "/genres", component: IndexGenres, exact: true },
-  { path: "/genres/create", component: CreateGenres },
-  { path: "/genres/edit/:id(\\d+)", component: EditGenres },
+  { path: "/genres", component: IndexGenres, exact: true, isAdmin: true },
+  { path: "/genres/create", component: CreateGenres, isAdmin: true },
+  { path: "/genres/edit/:id(\\d+)", component: EditGenres, isAdmin: true },
 
-  { path: "/actors", component: IndexActors, exact: true },
-  { path: "/actors/create", component: CreateActor },
-  { path: "/actors/edit/:id(\\d+)", component: EditActor },
+  { path: "/actors", component: IndexActors, exact: true, isAdmin: true },
+  { path: "/actors/create", component: CreateActor, isAdmin: true },
+  { path: "/actors/edit/:id(\\d+)", component: EditActor, isAdmin: true },
 
-  { path: "/moviecinemas", component: IndexMovieCinemas, exact: true },
-  { path: "/moviecinemas/create", component: CreateMovieCinema },
-  { path: "/moviecinemas/edit/:id(\\d+)", component: EditMovieCinema },
+  {
+    path: "/moviecinemas",
+    component: IndexMovieCinemas,
+    exact: true,
+    isAdmin: true,
+  },
+  { path: "/moviecinemas/create", component: CreateMovieCinema, isAdmin: true },
+  {
+    path: "/moviecinemas/edit/:id(\\d+)",
+    component: EditMovieCinema,
+    isAdmin: true,
+  },
 
-  { path: "/movies/create", component: CreateMovie },
-  { path: "/movies/edit/:id(\\d+)", component: EditMovie },
+  { path: "/movies/create", component: CreateMovie, isAdmin: true },
+  { path: "/movies/edit/:id(\\d+)", component: EditMovie, isAdmin: true },
   { path: "/movies/filter", component: FilterMovies },
   { path: "/movies/:id(\\d+)", component: MovieDetails },
 
