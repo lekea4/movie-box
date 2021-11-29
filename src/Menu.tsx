03e9752d3fd81193edb3d87e83d605fd1071afc5
Adeleke Ayinde <lekea4@gmail.com>
2021-11-29 10:10:56 +0100

    Modify the Menu  component to implement Authorization

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import Authorized from "./auth/Authorized";
 
 export default function Menu() {
   return (
@@ -10,34 +11,39 @@ export default function Menu() {
         <div className="collapse navbar-collapse">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <NavLink className="nav-link" to="/genres">
-                Genres
-              </NavLink>
-            </li>
-
-            <li className="nav-item">
               <NavLink className="nav-link" to="/movies/filter">
                 Filter Movies
               </NavLink>
             </li>
+            <Authorized
+              role="admin"
+              authorized={
+                <>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/actors">
+                      Actors
+                    </NavLink>
+                  </li>
 
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/actors">
-                Actors
-              </NavLink>
-            </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/moviecinemas">
+                      Cinema
+                    </NavLink>
+                  </li>
 
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/moviecinemas">
-                Cinema
-              </NavLink>
-            </li>
-
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/movies/create">
-                Create Movie
-              </NavLink>
-            </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/movies/create">
+                      Create Movie
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/genres">
+                      Genres
+                    </NavLink>
+                  </li>
+                </>
+              }
+            />
           </ul>
         </div>
       </div>
